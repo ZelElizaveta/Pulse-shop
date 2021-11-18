f3b0ae072cce7b025f6e869d477af2678b08066d
Elizaveta <lizastepanova93@mail.ru>
2021-11-18 21:29:48 +0700

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src ="icons/chevron_left.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src ="icons/chevron_right.png"></img></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src ="icons/chevron_right.png"></img></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+            }
+        }
+        ]
     });
   });
